Fixed gradients - working NN!

nn.py
@@ -1,6 +1,7 @@
 from random import random, shuffle
 import numpy as np
 from math import e, exp
+from statistics import mean
 
 
 def prepend_1(matrix):
@@ -98,7 +99,7 @@ class Layer:
         self.momentum = momentum
         self.learning_rate = learning_rate
         self.neuron_count = neuron_count
-        self.weights = np.matrix([[random() for _ in range(input_size)] for __ in range(neuron_count)])
+        self.weights = np.matrix([[random() / 100 for _ in range(input_size)] for __ in range(neuron_count)])
         self.last_inputs = None
         self.post_act_last_output = None
         self.pre_act_last_output = None
@@ -119,6 +120,7 @@ class Layer:
 
     def __repr__(self):
         return str(self)
+
 
 class NeuralNetwork:
     def __init__(self, input_count, hidden_layer_neuron_counts, hidden_layer_activations, output_count,
@@ -167,48 +169,73 @@ Layers: {}
         return str(self)
 
     @staticmethod
-    def cost(output, correct_output):
-        return [.5 * (o - c) ** 2 for o, c in zip(output, correct_output)]
+    def mse(output, correct_output):
+        return [(o - c) ** 2 for o, c in zip(output, correct_output)]
 
-    def train(self, data, answers, batch_size=0):
+    def train(self, data, answers, batch_size=0, shush=False):
         shufflable = list(zip(data, answers))
         shuffle(shufflable)
         count = 0
         wrong = 0
+        old_back_changes = None
+        old_middle_changes = None
+        old_output_changes = None
+        mses = []
         for datum, answer in shufflable:
             count += 1
             if batch_size:
                 if count > batch_size:
                     correct_count = batch_size - wrong
-                    print('{:.2f}%, {}/{} correct_count on training data'.format(correct_count / batch_size * 100, correct_count, batch_size))
-                    return
+                    if not shush:
+                        print('{:.2f}%, {}/{} correct_count on training data'.format(correct_count / batch_size * 100,
+                                                                                     correct_count, batch_size))
+                    return mean(mses)
             outputs, outputs_by_layer = self.process_input(datum)
             calc_answer = outputs.index(max(outputs))
 
             y = np.matrix([1 if i == answer else 0 for i in range(self.output_count)])
             y_hat = np.matrix(outputs)
+
+            mses.append(mean(NeuralNetwork.mse(vec_to_list(y_hat), vec_to_list(y))))
+
             if answer != calc_answer:
                 wrong += 1
 
                 output_layer = self.layers[-1]
-                pre_out = output_layer.pre_act_last_output
-                gradients = np.matrix([softmax_deriv(i, x_vec=pre_out) for i in range(output_layer.neuron_count)])
-                changes = output_layer.last_inputs * output_layer.learning_rate * gradients
+                dj_dy = j_y(y, y_hat)
+                dy_dz = y_z(output_layer.pre_act_last_output)
+                dj_dz = dy_dz * dj_dy.T
+                changes = dj_dz * output_layer.last_inputs.T * output_layer.learning_rate
 
                 output_layer_weights_wout_bias = output_layer.weights[:, 1:]
                 middle_layer = self.layers[-2]
-                middle_gradients = gradients.dot(output_layer_weights_wout_bias)
-                middle_changes = middle_layer.last_inputs * middle_layer.learning_rate * middle_gradients
+                middle_d_relu = np.matrix([0 if o < 0 else 1 for o in vec_to_list(middle_layer.pre_act_last_output)])
+                filtered_output_weights = np.multiply(middle_d_relu, output_layer_weights_wout_bias)
+                b = filtered_output_weights.T * dj_dz
+                middle_changes = (b * middle_layer.last_inputs.T) * middle_layer.learning_rate
 
-                if len(self.layers) > 2:
-                    middle_layer_weights_wout_bias = middle_layer.weights[:, 1:]
-                    back_layer = self.layers[-3]
-                    back_gradients = middle_gradients.dot(middle_layer_weights_wout_bias)
-                    back_changes = back_layer.last_inputs * back_layer.learning_rate * back_gradients
-                    back_layer.weights = back_layer.weights - back_changes.T
+                #if len(self.layers) > 2:
+                #    middle_layer_weights_wout_bias = middle_layer.weights[:, 1:]
+                #    back_layer = self.layers[-3]
+                #    back_d_relu = np.matrix([0 if o < 0 else 1 for o in vec_to_list(back_layer.pre_act_last_output)])
+                #    filtered_middle_weights = np.multiply(back_d_relu, middle_layer_weights_wout_bias)
+                #    dj_du = dj_dz.T * filtered_output_weights
+                #    dj_dv = dj_du * filtered_middle_weights
+                #    back_gradients = (back_layer.last_inputs * dj_dv).T
+                #    back_changes = back_gradients * back_layer.learning_rate
+                #    adjusted_back_changes = beta_changes(back_changes, old_back_changes, self.momentum)
+                #    back_layer.weights = back_layer.weights - adjusted_back_changes
+                #    old_back_changes = adjusted_back_changes
 
-                output_layer.weights = output_layer.weights - changes.T
-                middle_layer.weights = middle_layer.weights - middle_changes.T
+                adjusted_output_changes = beta_changes(changes, old_output_changes, self.momentum)
+                output_layer.weights = output_layer.weights - adjusted_output_changes
+                old_output_changes = adjusted_output_changes
+
+                adjusted_middle_changes = beta_changes(middle_changes, old_middle_changes, self.momentum)
+                middle_layer.weights = middle_layer.weights - adjusted_middle_changes
+                old_middle_changes = adjusted_middle_changes
+
+        return mean(mses)
 
     def test(self, test_data, test_answers, report=False, report_every=5, report_success=False):
         correct = 0
@@ -240,20 +267,41 @@ Layers: {}
         answer = output.index(max(output))
         return answer
 
+
+def beta_changes(w, old_w, momentum):
+    if momentum == 0 or old_w is None:
+        return w
+    else:
+        return w * (1 - momentum) + old_w * (momentum)
+
+
 def j_y(y, y_hat):
     return (-1 * y) / y_hat
 
-def y_z(y, z, l, z_i):
-    sum_e_z = sum([cached_exp(z_j) for z_j in z])
+
+def y_z_l_m(z, l, m, sum_e_z):
     e_z_l = cached_exp(z[0, l])
-    e_z_i = cached_exp(z[0, z_i])
-    if l == z_i:
+    e_z_m = cached_exp(z[0, m])
+    if l == m:
         return (sum_e_z * e_z_l - e_z_l ** 2) / sum_e_z ** 2
     else:
-        return (- e_z_l * e_z_i) / sum_e_z ** 2
+        return (- e_z_l * e_z_m) / sum_e_z ** 2
+
+
+def y_z(z):
+    sum_e_z = sum([cached_exp(z_j) for z_j in vec_to_list(z)])
+    return np.matrix([[y_z_l_m(z, l, m, sum_e_z) for l in range(z.shape[1])] for m in range(z.shape[1])])
+
 
 if __name__ == '__main__':
     y = np.matrix([1, 0])
     y_hat = np.matrix([.9, .1])
 
     print(j_y(y, y_hat))
+
+    z = [1, 2, 3]
+    outputs = np.matrix(softmax_func(z))
+    d_y_d_z = y_z(outputs)
+
+    print(outputs)
+    print(d_y_d_z)

num_driver_w_mse_graph.py
@@ -3,6 +3,7 @@ import random
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 from helpers import percentage
 from nn import NeuralNetwork, ActivationType
@@ -36,11 +37,21 @@ def double_shuffle(l1, l2):
     return l1, l2
 
 
-zs, os = 900, 900
+def read_num(i):
+    numfilename = 'num{}.csv'.format(i)
+    with open(numfilename, 'r') as infile:
+        lines = [[float(f) for f in l.split(',')] for l in infile.readlines()]
+    return lines
+
+
+zeros, ones = 900, 900
 zstest, ostest = 100, 100
 
-pre_rand_train_x = gendat(0, zs) + gendat(1, os)
-pre_rand_train_y = [0 for _ in range(900)] + [1 for _ in range(os)]
+num0 = read_num(0)
+num1 = read_num(1)
+pre_rand_train_x = num0 + num1
+pre_rand_train_y = [0 for _ in range(len(num0))] + [1 for _ in range(len(num1))]
+
 rand_x, rand_y = double_shuffle(pre_rand_train_x, pre_rand_train_y)
 
 X_train = np.matrix(rand_x)
@@ -56,13 +67,10 @@ print('X_test={}'.format(len(X_test)))
 
 neurons = [5]
 activation = [ActivationType.relu, ActivationType.relu]
-batch_size = None
 momentum = 0.0
 learning_rate = .000005
-graphing_mse = True
-iterations = 100
-append_params(neurons=neurons, learning_rate=learning_rate, batch_size=batch_size, momentum=momentum,
-              iterations=iterations)
+iterations = 150
+append_params(neurons=neurons, learning_rate=learning_rate, momentum=momentum)
 nn = NeuralNetwork(2, neurons, activation, 2, momentum=momentum, learning_rate=learning_rate)
 
 with Timer(lambda t: print('Took {} seconds'.format(t))):
@@ -74,10 +82,13 @@ for i in range(iterations):
     if i % 10 == 0 or i == 0:
         accuracy = nn.test(X_test.tolist(), y_test.tolist()[0])
         append_progress(i, accuracy)
-        print('Accuracy: {}'.format(percentage(accuracy)))
-    mean_mse = nn.train(X_train.tolist(), y_train.tolist()[0], batch_size=batch_size, shush=True)
+        print('{}/{} Accuracy: {}'.format(i, iterations, percentage(accuracy)))
+    mean_mse = nn.train(X_train.tolist(), y_train.tolist()[0], shush=True)
+    print(mean_mse)
     mean_mses.append(mean_mse)
 
 plt.plot(mean_mses)
-plt.suptitle('Mean MSE over time')
-plt.show()
+plt.suptitle('Mean MSE over time B={}'.format(momentum))
+figname = './mseb{}.png'.format(int(momentum * 10))
+plt.savefig(figname)
+print(mean_mses)